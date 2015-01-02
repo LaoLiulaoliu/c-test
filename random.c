3f4c52cd58eb34b46e232062fb199e290d4bf928
yuande <q_0_0_p@163.com>
2015-01-02 17:19:33 +0800

    refactor random

--- random.c
+++ random.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+int randrange(int start, int end);
+int randint(int start, int end);
+
 int main()
 {
     int max = 20, min = -80;
@@ -14,11 +17,18 @@ int main()
     float i = (float)rand / (RAND_MAX + 1.0) * (max - min) + min; //rational number
     int j = rand % (max - min + 1) + min; // integer
     printf("%f, %d\n", i, j);
+
     return 0;
 }
 
+
+#include <signal.h>
 int randrange(int start, int end)
 {
+    if(start > end) {
+        raise(SIGFPE);
+    }
+
     /* generate random integer in [start, end) */
     srandom( (unsigned)time(NULL) );
     return random() % (end - start + 1) + start;
@@ -27,5 +37,5 @@ int randrange(int start, int end)
 int randint(int start, int end)
 {
     /* generate random integer in [start, end] */
-    return randrange(start, end+1)
+    return randrange(start, end+1);
 }
